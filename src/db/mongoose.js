1af70cdb7fceb8e22923f2b11f509c263ca3d34a
Nakovanj <nakovanj@gmail.com>
2023-12-18 00:21:27 +0100

    [task-api] log url

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-
+console.log("URL: ", process.env.MONGODB_URL)
 mongoose.connect(process.env.MONGODB_URL, {
     dbName: 'task-manager-api',
     useNewUrlParser: true,
