b4f3e222a09be8d9482466bfe79920c3b8112483
Nakovanj <nakovanj@gmail.com>
2023-12-18 01:13:54 +0100

    [task api] new uri parser

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 console.log("URL: ", process.env.MONGODB_URL)
 mongoose.connect(process.env.MONGODB_URL, {
     dbName: 'task-manager-api',
-    useNewUrlParser: false,
+    useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true
